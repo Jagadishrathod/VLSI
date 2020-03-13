14327c1705d732489e3dc94ce6250d590a17833a
rathod jagadish <46041625+Jagadishrathod@users.noreply.github.com>
2020-03-13 19:42:32 +0530

    Update FullAdder8.v

--- Wallace16bit/FullAdder8.v
+++ Wallace16bit/FullAdder8.v
@@ -1,15 +1,12 @@
 `include "FullAdder4_2.v"
 
 module FullAdder8 (A,B,Cin,Sum,Carry);
-
 	input [7:0] A,B;
 	input Cin;
-
 	output [7:0] Sum;
 	output Carry;
 	wire w0;
+	
 	FullAdder4_2 uut_1 (A[3:0],B[3:0],Cin,Sum[3:0],w0);
 	FullAdder4_2 uut_2 (A[7:4],B[7:4],w0,Sum[7:4],Carry);
-	
-
 endmodule
